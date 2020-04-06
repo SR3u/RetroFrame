ad49f53a93f9c628a81bff78c2d0d49c71610e43
sr3u <wnmsr3u@gmail.com>
2020-04-06 16:37:55 +0300

    Refreshing mediaItems in Event

--- src/main/java/org/sr3u/photoframe/settings/Fillable.java
+++ src/main/java/org/sr3u/photoframe/settings/Fillable.java
@@ -5,31 +5,34 @@ import java.util.Properties;
 
 public interface Fillable {
     default void fill(Properties properties) {
-        try {
-            Field[] fields = this.getClass().getDeclaredFields();
-            for (Field field : fields) {
+        Field[] fields = this.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            try {
                 if (field.isAnnotationPresent(PropertyMap.class)) {
                     field.setAccessible(true);
                     PropertyMap annotation = field.getAnnotation(PropertyMap.class);
                     String s = (String) properties.get(annotation.value());
-                    Class<?> type = field.getType();
-                    if (Boolean.class.equals(type) || boolean.class.equals(type)) {
-                        field.set(this, "true".equalsIgnoreCase(s));
-                    } else if (String.class.equals(type)) {
-                        field.set(this, s);
-                    } else if (Integer.class.equals(type) || int.class.equals(type)) {
-                        field.set(this, Integer.valueOf(s));
-                    } else if (Long.class.equals(type) || long.class.equals(type)) {
-                        field.set(this, Long.valueOf(s));
-                    } else if (Double.class.equals(type) || double.class.equals(type)) {
-                        field.set(this, Double.valueOf(s));
-                    } else {
-                        throw new Exception("Unsupported type: " + type);
+                    if (s != null) {
+                        Class<?> type = field.getType();
+                        if (Boolean.class.equals(type) || boolean.class.equals(type)) {
+                            field.set(this, "true".equalsIgnoreCase(s));
+                        } else if (String.class.equals(type)) {
+                            field.set(this, s);
+                        } else if (Integer.class.equals(type) || int.class.equals(type)) {
+                            field.set(this, Integer.valueOf(s));
+                        } else if (Long.class.equals(type) || long.class.equals(type)) {
+                            field.set(this, Long.valueOf(s));
+                        } else if (Double.class.equals(type) || double.class.equals(type)) {
+                            field.set(this, Double.valueOf(s));
+                        } else {
+                            throw new Exception("Unsupported type: " + type);
+                        }
                     }
                 }
+            } catch (Exception e) {
+                System.out.println(getClass().getName() + "." + field.getName());
+                throw new RuntimeException(e);
             }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
     }
 
