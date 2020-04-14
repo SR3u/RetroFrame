6566245b19f60118550bdb91e3b171cf461bd3c3
sr3u <wnmsr3u@gmail.com>
2020-04-14 19:32:38 +0300

    Better About

--- src/main/java/org/sr3u/photoframe/client/PopupMenu.java
+++ src/main/java/org/sr3u/photoframe/client/PopupMenu.java
@@ -7,23 +7,19 @@ public class PopupMenu extends JPopupMenu {
     public PopupMenu(ImageWindow mainWindow) {
         addItem("Settings", e -> JOptionPane.showMessageDialog(null,
                 "Not implemented yet\n" +
-                        "Please edit settings.properties file located in "+System.getProperty("user.dir")+"\n" +
+                        "Please edit settings.properties file located in " + System.getProperty("user.dir") + "\n" +
                         "And then restart the app",
                 "Settings",
                 JOptionPane.ERROR_MESSAGE));
         if (mainWindow.isFullScreen()) {
-            JMenuItem item = createItem("Exit FullScreen", e -> exitFullscreen(mainWindow));
-            item.setMnemonic(KeyEvent.VK_ENTER);
-            add(item);
+            addItem("Exit FullScreen", e -> exitFullscreen(mainWindow));
         } else {
-            JMenuItem item = createItem("FullScreen", e -> enterFullscreen(mainWindow));
-            item.setMnemonic(KeyEvent.VK_ENTER);
-            add(item);
+            addItem("FullScreen", e -> enterFullscreen(mainWindow));
         }
         addItem("About", e -> JOptionPane.showMessageDialog(null,
                 "Authors:\n" +
                         "   SR3u    -- code\n" +
-                        "   4113556 -- logo design",
+                        "   4113556 -- logo design, testing, and other help",
                 "About " + ImageWindow.TITLE_NAME,
                 JOptionPane.INFORMATION_MESSAGE));
     }
