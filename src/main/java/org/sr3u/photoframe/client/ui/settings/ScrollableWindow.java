69b9eb4887a09103d51af04696e0991f8cbae544
sr3u <wnmsr3u@gmail.com>
2020-04-16 18:43:50 +0300

    Filters screen draft

--- src/main/java/org/sr3u/photoframe/client/ui/settings/ScrollableWindow.java
+++ src/main/java/org/sr3u/photoframe/client/ui/settings/ScrollableWindow.java
@@ -1,18 +1,19 @@
 package org.sr3u.photoframe.client.ui.settings;
 
+import org.sr3u.photoframe.client.ui.ClientWindow;
+
 import javax.swing.*;
 import java.awt.*;
 
-public abstract class ScrollableWindow {
+public abstract class ScrollableWindow extends ClientWindow {
 
     public static final Dimension MINIMUM_SIZE = new Dimension(200, 200);
 
-    protected final JFrame frame;
     protected final JScrollPane scrollPane;
     protected final JPanel scrollPaneContent;
 
     public ScrollableWindow(String title) {
-        frame = new JFrame();
+        super();
         frame.setTitle(title);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setMinimumSize(new Dimension(400, (int) MINIMUM_SIZE.getWidth()));
