e19adffdf71a16b68828367b9f076caf4b7aeaa8
sr3u <wnmsr3u@gmail.com>
2020-04-18 15:38:16 +0300

    Better refresh timeout logic

--- src/main/java/org/sr3u/photoframe/client/ClientThread.java
+++ src/main/java/org/sr3u/photoframe/client/ClientThread.java
@@ -16,12 +16,17 @@ import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ClientThread extends Thread {
 
     private static final Logger log = LogManager.getLogger(ClientThread.class);
 
     public static final Gson GSON = new Gson();
+
+    private final ScheduledExecutorService execService = Executors.newSingleThreadScheduledExecutor();
     private ImageWindow imageWindow;
 
     private static Socket clientSocket;
@@ -47,57 +52,70 @@ public class ClientThread extends Thread {
 
     @Override
     public void run() {
-        while (true) {
+        updateImage();
+        scheduleRefresh();
+    }
+
+    private void scheduleRefresh() {
+        execService.schedule(this::updateImage, Main.settings.getClient().getRefreshDelay(), TimeUnit.MILLISECONDS);
+    }
+
+    private void updateImage() {
+        try {
             try {
-                try {
-                    clientSocket = new Socket(host, port);
-                    in = new BufferedInputStream(clientSocket.getInputStream());
-                    out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-                    out.flush();
-                    Dimension size = imageWindow.getSize();
-                    out.write(size.width + "x" + size.height + "x" + 32);
-                    out.write(0);
-                    out.flush();
-                    int metadataSize = Main.intFromByteArray(in.readNBytes(4));
-                    String json = new String(in.readNBytes(metadataSize));
-                    Map<String, Object> metaData = GSON.<Map>fromJson(json, Map.class);
-                    int imageSize = Main.intFromByteArray(in.readNBytes(4));
-                    log.info("Metadata: " + json);
-                    imageWindow.displayImageAndMetadata(in, metaData);
-                } catch (Exception e) {
-                    log.error(e);
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        if (in != null) {
-                            in.close();
-                        }
-                    } catch (Exception e) {
-                        log.error(e);
-                        e.printStackTrace();
-                    }
-                    try {
-                        if (out != null) {
-                            out.close();
-                        }
-                    } catch (Exception e) {
-                        log.error(e);
-                        e.printStackTrace();
-                    }
-                    try {
-                        if (clientSocket != null) {
-                            clientSocket.close();
-                        }
-                    } catch (Exception e) {
-                        log.error(e);
-                        e.printStackTrace();
-                    }
-                    Thread.sleep(Main.settings.getClient().getRefreshDelay());
-                }
+                clientSocket = new Socket(host, port);
+                in = new BufferedInputStream(clientSocket.getInputStream());
+                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                out.flush();
+                Dimension size = imageWindow.getSize();
+                out.write(size.width + "x" + size.height + "x" + 32);
+                out.write(0);
+                out.flush();
+                int metadataSize = Main.intFromByteArray(in.readNBytes(4));
+                String json = new String(in.readNBytes(metadataSize));
+                Map<String, Object> metaData = parseMetadata(json);
+                int imageSize = Main.intFromByteArray(in.readNBytes(4));
+                log.info("Metadata: " + json);
+                imageWindow.displayImageAndMetadata(in, metaData);
             } catch (Exception e) {
                 log.error(e);
                 e.printStackTrace();
+            } finally {
+                try {
+                    if (in != null) {
+                        in.close();
+                    }
+                } catch (Exception e) {
+                    log.error(e);
+                    e.printStackTrace();
+                }
+                try {
+                    if (out != null) {
+                        out.close();
+                    }
+                } catch (Exception e) {
+                    log.error(e);
+                    e.printStackTrace();
+                }
+                try {
+                    if (clientSocket != null) {
+                        clientSocket.close();
+                    }
+                } catch (Exception e) {
+                    log.error(e);
+                    e.printStackTrace();
+                }
             }
+        } catch (Exception e) {
+            log.error(e);
+            e.printStackTrace();
+        } finally {
+            scheduleRefresh();
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    private Map<String, Object> parseMetadata(String json) {
+        return GSON.fromJson(json, Map.class);
     }
 }
