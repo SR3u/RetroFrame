899ab72f2b0c7df5844a0c51ada7822c3a800460
SR3u <wnmsr3u@gmail.com>
2023-05-16 20:35:31 +0300

    DockerFile

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git pull origin --ff-only
 mvn clean install -Dmaven.test.skip=true
+chmod 0777 ./target/retroframe.jar
 docker image rm sr3u/retroframe:latest
 docker build -t sr3u/retroframe:latest ./
