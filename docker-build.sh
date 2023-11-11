eef1e33e0cf8ec975e58f5494908112d5cdae19f
SR3u <wnmsr3u@gmail.com>
2023-11-11 16:23:01 +0300

    updated docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+timestamp=$(date +%Y%m%d%H%M%S)
 git pull origin --ff-only
 mvn clean install -Dmaven.test.skip=true
 chmod 0777 ./target/retroframe.jar
-docker image rm sr3u/retroframe:latest
-docker build -t sr3u/retroframe:latest ./
+docker image rm sr3u/retroframe:$timestamp
+docker build -t sr3u/retroframe:$timestamp ./
+docker push sr3u/retroframe:$timestamp
+docker image rm sr3u/retroframe:$timestamp
